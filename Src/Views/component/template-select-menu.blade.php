111020403e2fb0654cecb1e1efb585cbf2415489
muction <muction@yeah.net>
2020-01-15 18:51:30 +0800

    dev code

--- Src/Views/component/template-select-menu.blade.php
+++ Src/Views/component/template-select-menu.blade.php
@@ -1,11 +1,11 @@
 @foreach( $datas as $key=>$item)
-    <option value="{{$item['template_name']}}" @if( !$item['template_name'] ) disabled="disabled"  @endif
-        @if($templateName == $item['template_name']) selected="selected" @endif
-    >
-        @for($i=0;$i< ($item['level']-1) *3;$i++) &nbsp; @endfor {{ $item['title'] }}
-    </option>
+    <li>
+        <a href="{{ route('rotate.template.index' , ['nav'=>$validNavId ,'template_name'=>$item['template_name'] ]) }}">
+            @for($i=0;$i< ($item['level']-1) *3;$i++) &nbsp; @endfor {{ $item['title'] }}
+        </a>
+    </li>
 
     @if( isset($item['nodes']) )
-        @component('StarsPeace::component.template-select-menu', ['datas'=> $item['nodes'] ,'templateName'=>$templateName])  @endcomponent
+        @component('StarsPeace::component.template-select-menu', ['datas'=> $item['nodes'] ,'templateName'=>$templateName ,'validNavId'=>$validNavId])  @endcomponent
     @endif
  @endforeach
--- Src/Views/template/index.blade.php
+++ Src/Views/template/index.blade.php
@@ -4,7 +4,7 @@
 @section("page-head")
     <link rel="stylesheet" type="text/css" href="{{ asset("static/stars/plugs/codemirror/lib/codemirror.css") }}"/>
     <link rel="stylesheet" type="text/css" href="{{ asset("static/stars/plugs/codemirror/theme/night.css") }}"/>
-    <link rel="stylesheet" type="text/css" href="{{ asset("static/stars/plugs/codemirror/theme/ambiance.css") }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset("static/stars/plugs/codemirror/theme/mdn-like.css") }}"/>
     <script type="text/javascript" src="{{ asset("static/stars/plugs/codemirror/lib/codemirror.js") }}"></script>
     <script type="text/javascript" src="{{ asset("static/stars/plugs/codemirror/addon/selection/active-line.js") }}"></script>
     <script type="text/javascript" src="{{ asset("static/stars/plugs/codemirror/addon/edit/matchbrackets.js") }}"></script>
@@ -16,73 +16,73 @@
     <script type="text/javascript" src="{{ asset("static/stars/plugs/codemirror/mode/php/php.js") }}"></script>
 
     <style type="text/css">
-        .list-group-item  a{
-            color: #3a3432;
-        }
-
-        .active  a{
-            color: #fff;
-        }
     </style>
 @endsection
 
 @section('car-body')
         <div class="row">
-            <div class="col-md-2">
-                <form action="" method="get" id="templateForm">
+            <div class="col-md-3" style="padding-right: 0px;">
+                <div class="panel panel-default">
+                    <div class="panel-heading">{{ $templateDataSource }}</div>
+                    <div class="panel-body">
+                        <form action="" method="get" id="templateForm">
 
-                    {{--  所有模板文件 --}}
-
-                        <div class="row">
-                            <div class="col-md-3">
-                                <select name="nav" class="">
-                                    @foreach($articleNavs as $nav)
-                                        <option value="{{ $nav['id'] }}" @if($validNavId == $nav['id'] ) selected="selected" @endif>
-                                            {{ $nav['title'] }}
-                                        </option>
-                                    @endforeach
-                                </select>
+                            {{--  所有模板文件 --}}
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="btn-group " role="group" aria-label="...">
+                                        <div class="btn-group" role="group">
+                                            <button type="button" class="btn btn-sm btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                选择导航
+                                                <span class="caret"></span>
+                                            </button>
+                                            <ul class="dropdown-menu">
+                                                @foreach($articleNavs as $nav)
+                                                    <li value="{{ $nav['id'] }}">
+                                                        <a href="{{ route('rotate.template.index' , ['nav'=>$nav ,'template_name'=>$templateName ]) }}">
+                                                            {{ $nav['title'] }}
+                                                        </a>
+                                                    </li>
+                                                @endforeach
+                                            </ul>
+                                        </div>
+                                        <div class="btn-group" role="group">
+                                            <button type="button" class="btn btn-sm btn-default btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                选择菜单
+                                                <span class="caret"></span>
+                                            </button>
+                                            <ul class="dropdown-menu">
+                                                @component('StarsPeace::component.template-select-menu', ['datas'=> $navMenus ,'templateName'=>$templateName ,'validNavId'=>$validNavId ]) @endcomponent
+                                            </ul>
+                                        </div>
+                                        <button type="button" class="btn btn-sm btn-danger btn-default" id="btn-apply-change">应用更改</button>
+                                        <button type="button" class="btn  btn-sm btn-dark btn-default" id="btn-back-last-version">向上回滚</button>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-md-7">
-                                <select name="template_name" class="">
-                                    @component('StarsPeace::component.template-select-menu', ['datas'=> $navMenus ,'templateName'=>$templateName ]) @endcomponent
-                                </select>
+
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <ol style="margin-top: 10px;">
+                                        @foreach($templateFiles as  $index=>$filePathName)
+                                            @if( strstr( $filePathName , $validNavInfo['theme'] ) )
+                                                <li>
+                                                    <a href="{{ route('rotate.template.index' , ['nav'=>$validNavId ,'template_name'=>$filePathName ]) }}"
+                                                       style="@if($templateName == $filePathName) color:green @else color:#9C9E9E @endif "
+                                                       style="line-height: 10px" >
+                                                        {{ $filePathName }}
+                                                    </a>
+                                                </li>
+                                            @endif
+                                        @endforeach
+                                    </ol>
+                                </div>
                             </div>
-                        </div>
-
-
-                        <div class="" style="height: 10px;"></div>
-
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <button class="btn btn-sm btn-warning" type="button" id="btn-apply-change">应用更改</button>
-                                <button class="btn btn-sm btn-default" type="button" id="btn-back-last-version">回滚</button>
-
-                                <label class="label label-info" title="模板数据来源">{{$templateDataSource}}</label>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <ol>
-                                    @foreach($templateFiles as  $index=>$filePathName)
-                                        @if( strstr( $filePathName , $validNavInfo['theme'] ) )
-                                            <li>
-                                                <a href="{{ route('rotate.template.index' , ['nav'=>$validNavId ,'template_name'=>$filePathName ]) }}"
-                                                   style="@if($templateName == $filePathName) color:red @endif "
-                                                   style="line-height: 10px"
-                                                >
-                                                   {{ $filePathName }}
-                                                </a>
-                                            </li>
-                                        @endif
-                                    @endforeach
-                                </ol>
-                            </div>
-                        </div>
-                </form>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-10">
+            <div class="col-md-9" style="padding-left: 0px;">
                 <textarea name="templateContent" id="code"  >{{$templateContent}}</textarea>
             </div>
         </div>
@@ -104,9 +104,7 @@
                 styleActiveLine: true,
                 matchBrackets: true,
                 lineWrapping: true,
-                theme: "ambiance"
-                //mode:  "php" ,
-                //setSize : {width: 200, height: 600 }
+                theme: "mdn-like"
             });
 
             var documentHeight = $(document).height();
