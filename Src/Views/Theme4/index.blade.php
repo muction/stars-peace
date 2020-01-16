9a9baa360106390623d04a9906b6299358f9669c
muction <muction@yeah.net>
2020-01-16 18:36:54 +0800

    dev

--- Src/Views/Theme4/index.blade.php
+++ Src/Views/Theme4/index.blade.php
@@ -310,19 +310,18 @@
                 </div>
                 <nav class="mt-nav mt-nav-panel" style="width: calc(100% - 120px);">
                     <ul class="nav nav-tabs" style="margin-left: 0px;">
-                        <li class=""><a data-id="multitabs_main_0" class="mt-nav-tab" data-type="main" data-index="0"
-                                        data-url="lyear_main.html">首页 </a></li>
-                        <li class="active"><a data-id="multitabs_info_1" class="mt-nav-tab" data-type="info"
-                                              data-index="1" data-url="lyear_forms_radio.html">单选框 </a> <i
-                                class="mt-close-tab mdi mdi-close" style="display:inline;"></i></li>
+                        <li class="active">
+                            <a data-id="multitabs_main_0" class="mt-nav-tab" data-type="main" data-index="0"
+                                        href="http://www.baidu.com" target="request-content">
+                                首页
+                            </a>
+                        </li>
                     </ul>
                 </nav>
 
             </div>
             <div class="container-fluid">
-
                 <iframe src="http://xin.com" name="request-content" width="100%" height="100%"></iframe>
-
             </div>
 
         </main>
@@ -335,52 +334,5 @@
 <script type="text/javascript" src="{{ asset('static/stars/js/perfect-scrollbar.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('static/stars/js/main.min.js') }}"></script>
 
-<!--图表插件-->
-<script type="text/javascript" src="{{ asset('static/stars/js/Chart.js') }}"></script>
-<script type="text/javascript">
-    $(document).ready(function (e) {
-        var $dashChartBarsCnt = jQuery('.js-chartjs-bars')[0].getContext('2d'),
-            $dashChartLinesCnt = jQuery('.js-chartjs-lines')[0].getContext('2d');
-
-        var $dashChartBarsData = {
-            labels: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
-            datasets: [
-                {
-                    label: '注册用户',
-                    borderWidth: 1,
-                    borderColor: 'rgba(0,0,0,0)',
-                    backgroundColor: 'rgba(51,202,185,0.5)',
-                    hoverBackgroundColor: "rgba(51,202,185,0.7)",
-                    hoverBorderColor: "rgba(0,0,0,0)",
-                    data: [2500, 1500, 1200, 3200, 4800, 3500, 1500]
-                }
-            ]
-        };
-        var $dashChartLinesData = {
-            labels: ['2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014'],
-            datasets: [
-                {
-                    label: '交易资金',
-                    data: [20, 25, 40, 30, 45, 40, 55, 40, 48, 40, 42, 50],
-                    borderColor: '#358ed7',
-                    backgroundColor: 'rgba(53, 142, 215, 0.175)',
-                    borderWidth: 1,
-                    fill: false,
-                    lineTension: 0.5
-                }
-            ]
-        };
-
-        new Chart($dashChartBarsCnt, {
-            type: 'bar',
-            data: $dashChartBarsData
-        });
-
-        var myLineChart = new Chart($dashChartLinesCnt, {
-            type: 'line',
-            data: $dashChartLinesData,
-        });
-    });
-</script>
 </body>
 </html>
