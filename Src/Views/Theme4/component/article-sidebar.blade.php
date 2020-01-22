c7d369fe283797d353c6ffc1c741785e9a470234
muction <muction@yeah.net>
2020-01-22 16:35:31 +0800

    dev

--- Src/Views/Theme4/component/article-sidebar.blade.php
+++ Src/Views/Theme4/component/article-sidebar.blade.php
@@ -4,13 +4,13 @@
     @if( !isset($side['nodes']) || !$side['nodes'] )
         @if( isset($children) )
             <li>
-                <a target="request-content" href="{{ isset($side['url']) ? $side['url'] : 'javascript:void(0)'}}">
+                <a target="request-content" data-id="1" @if(!isset($side['url'])) style="text-decoration: underline; cursor: not-allowed;" class="disabled" @endif href="{{ isset($side['url']) ? $side['url'] : 'javascript:void(0)'}}">
                     {{ $side['title'] }}
                 </a>
             </li>
         @elseif( \Stars\Rbac\Entity\UserEntity::can( $side['route_name']) ||  \Stars\Rbac\Entity\UserEntity::hasRole( 'root') )
             <li class="nav-item">
-                <a target="request-content" href="{{ isset($side['url']) ? $side['url'] : 'javascript:void(0)'}}" >
+                <a target="request-content" data-id="2" @if(!isset($side['url'])) style="text-decoration: underline; cursor: not-allowed;" class="disabled" @endif href="{{ isset($side['url']) ? $side['url'] : 'javascript:void(0)'}}" >
                     <i class="{{ $side['icon'] }}"></i>
                     <span>{{ $side['title'] }}</span>
                 </a>
@@ -20,10 +20,16 @@
     @else
 
         @if( \Stars\Rbac\Entity\UserEntity::can( $side['route_name']) ||  \Stars\Rbac\Entity\UserEntity::hasRole( 'root') )
-            <li class="nav-item nav-item-has-subnav">
-                <a href="{{ isset($side['url']) ? $side['url'] : 'javascript:void(0)'}}" target="request-content">
+            <li class="nav-item nav-item-has-subnav" >
+                <a href="{{ isset($side['url']) ? $side['url'] : 'javascript:void(0)'}}"
+                   target="request-content"
+                   @if(!isset($side['url'])) title="未绑定可操作项" style="text-decoration: underline; cursor: not-allowed;" @endif
+                   data-id="3"
+                >
                     <i class="{{ $side['icon'] }}"></i>
-                    <span>{{ $side['title'] }}</span>
+
+                    <span >{{ $side['title'] }}</span>
+
                 </a>
                 <ul class="nav nav-subnav">
                     @component( "StarsPeace::component.article-sidebar" ,['sides'=>$side['nodes'] ,'children'=>time()  ] ) @endcomponent
--- Src/Views/Theme4/template/index.blade.php
+++ Src/Views/Theme4/template/index.blade.php
@@ -3,16 +3,21 @@
 
 @section("page-head")
     <link rel="stylesheet" type="text/css" href="{{ asset("static/stars/plugs/codemirror/lib/codemirror.css") }}"/>
-    <link rel="stylesheet" type="text/css" href="{{ asset("static/stars/plugs/codemirror/theme/night.css") }}"/>
     <link rel="stylesheet" type="text/css" href="{{ asset("static/stars/plugs/codemirror/theme/mdn-like.css") }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset("static/stars/plugs/codemirror/theme/the-matrix.css") }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset("static/stars/plugs/select2/css/select2.css") }}"/>
+    <script type="text/javascript" src="{{ asset("static/stars/plugs/select2/js/select2.js") }}"></script>
+
     <script type="text/javascript" src="{{ asset("static/stars/plugs/codemirror/lib/codemirror.js") }}"></script>
-    <script type="text/javascript" src="{{ asset("static/stars/plugs/codemirror/addon/selection/active-line.js") }}"></script>
-    <script type="text/javascript" src="{{ asset("static/stars/plugs/codemirror/addon/edit/matchbrackets.js") }}"></script>
 
-
+    <script type="text/javascript"
+            src="{{ asset("static/stars/plugs/codemirror/addon/selection/active-line.js") }}"></script>
+    <script type="text/javascript"
+            src="{{ asset("static/stars/plugs/codemirror/addon/edit/matchbrackets.js") }}"></script>
     <script type="text/javascript" src="{{ asset("static/stars/plugs/codemirror/mode/stylus/stylus.js") }}"></script>
     <script type="text/javascript" src="{{ asset("static/stars/plugs/codemirror/mode/css/css.js") }}"></script>
-    <script type="text/javascript" src="{{ asset("static/stars/plugs/codemirror/mode/javascript/javascript.js") }}"></script>
+    <script type="text/javascript"
+            src="{{ asset("static/stars/plugs/codemirror/mode/javascript/javascript.js") }}"></script>
     <script type="text/javascript" src="{{ asset("static/stars/plugs/codemirror/mode/php/php.js") }}"></script>
 
     <style type="text/css">
@@ -22,11 +27,22 @@
 @section("car-head")
 
     @php( $_routeName = Request()->route()->getName() )
-    <ul  class="nav nav-tabs" >
+    <ul class="nav nav-tabs">
         <li>
-            <form id="templateForm" method="get">
+            <a  id="btn-back-last-version">向上回滚</a>
+        </li>
+        <li>
+            <a  id="btn-apply-change">应用更改</a>
+        </li>
+    </ul>
+@endsection
 
-                <select id="nav-select" name="nav"  style="margin-top: 8px;margin-left: 15px">
+@section('car-body')
+    <div class="row" style="height: 100%" >
+        <div class="col-md-2" style="padding-right: 0px;padding-left: 0px">
+
+            <form id="templateForm" method="get">
+                <select id="nav-select" name="nav" class="select23" style=" width: 100px;margin-top: 8px;margin-left: 15px">
                     @foreach($articleNavs as $nav)
                         <option value="{{ $nav['id'] }}"
                                 @if( $validNavId ==  $nav['id'] )
@@ -37,142 +53,131 @@
                     @endforeach
                 </select>
 
-                <select id="nav-menu-select" name="template_name" style="width: 150px">
+                <select id="nav-menu-select" class="select23" name="template_name" style="width: 150px">
                     @component('StarsPeace::component.template-select-menu', ['datas'=> $navMenus ,'templateName'=>$templateName ,'validNavId'=>$validNavId ]) @endcomponent
                 </select>
             </form>
 
-        </li>
-
-        <li>
-            <div style="margin: 10px;" class="text-primary"> {{ $templateDataSource ? $templateDataSource.' :'.$templateName : '请选择要编辑的模板' }} </div>
-        </li>
-
-        <li>
-            <div class="btn-group" role="group" style="margin-top:2px; margin-left: 5px">
-                <button type="button" class="btn btn-default" id="btn-back-last-version">向上回滚</button>
-                <button type="button" class="btn btn-default" id="btn-apply-change">应用更改</button>
-            </div>
-        </li>
-    </ul>
-@endsection
-
-@section('car-body')
-    <div class="row" style="height: 100%">
-        <div class="col-md-3" style="padding-right: 0px;padding-left: 0px">
-            <ol>
+            <div id="box-height" class="list-group" style="margin-top: 10px;margin-right: 14px">
                 @foreach($templateFiles as  $index=>$filePathName)
                     @if( strstr( $filePathName , $validNavInfo['theme'] ) )
-                        <li>
+
                             <a href="{{ route('rotate.template.index' , ['nav'=>$validNavId ,'template_name'=>$filePathName ]) }}"
-                               style="@if($templateName == $filePathName) color:green @else color:#9C9E9E @endif "
-                               style="line-height: 10px" >
+                               style="line-height: 10px;text-align: right"
+                               class="list-group-item  @if($templateName == $filePathName) active  @endif"
+                            >
                                 {{ $filePathName }}
                             </a>
-                        </li>
+
                     @endif
                 @endforeach
-            </ol>
+            </div>
         </div>
-        <div class="col-md-9" style="padding-left: 0px;">
-            <textarea name="templateContent" id="code"  >{{$templateContent}}</textarea>
+        <div class="col-md-10" style="padding-left: 0px;">
+            <textarea name="templateContent" id="code">{{$templateContent}}</textarea>
         </div>
     </div>
 
     <script type="text/javascript">
-        $(function(){
-            $("select").change(function(){
+        $(function () {
+            $("select").change(function () {
                 $('#templateForm').submit();
             });
+
+            $('.select23').select2();
         });
 
 
+        var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+            lineNumbers: true,
+            styleActiveLine: true,
+            matchBrackets: true,
+            lineWrapping: true,
+            theme: "mdn-like"
+        });
+
+        var documentHeight = $('#box-height').height();
+        editor.setSize('auto', documentHeight);
+        $(function () {
+            let templateName = "{{ $templateName }}";
+            let validNavId = "{{$validNavId}}";
+            let token = "{{csrf_token()}}";
+            var hasRequest = null;
+            //应用本次更改
+            $('#btn-apply-change').click(function () {
+
+                if (confirm('您即将应用本次更改，将会替换现有模板文件，是否继续?') && confirm("确定继续操作?")) {
+                    if (hasRequest != null) {
+                        hasRequest.abort();
+                    }
+
+                    hasRequest = $.ajax({
+                        url: "{{ route('rotate.template.apply') }}",
+                        type: "post",
+                        data: {
+                            templateName: templateName,
+                            validNavId: validNavId,
+                            templateContent: editor.getValue(),
+                            _token: token
+                        },
+                        dataType: "json",
+                        error: function () {
+                            alert("应用时发生错误，您可以稍后尝试重试，如果错误继续存在，请联系站点管理员~");
+                        },
+                        success: function (e) {
+                            if (e.error == 0) {
+                                alert(e.msg);
+                                window.location.reload();
+                            } else {
+                                alert('操作失败了');
+                            }
+                        }
+                    });
+                }
+            });
+
+            $('#btn-back-last-version').click(function () {
+                if (confirm('确定要回滚到上次版本吗? 是否继续?') && confirm("确定继续操作?")) {
+                    let backVersion = "";
+                    while (!backVersion) {
+                        backVersion = window.prompt("请输入要回归的版本，__LAST__ 将回滚到上次版本", "__LAST__");
+                        if (backVersion == null) {
+                            break;
+                        }
+                    }
+
+                    if (!backVersion) {
+                        return false;
+                    }
+
+                    if (hasRequest != null) {
+                        hasRequest.abort();
+                    }
+
+                    hasRequest = $.ajax({
+                        url: "{{ route('rotate.template.rollBack') }}",
+                        type: "post",
+                        data: {
+                            backVersion: backVersion,
+                            templateName: templateName,
+                            validNavId: validNavId,
+                            templateContent: editor.getValue(),
+                            _token: token
+                        },
+                        dataType: "json",
+                        error: function () {
+                            alert("应用时发生错误，您可以稍后尝试重试，如果错误继续存在，请联系站点管理员~");
+                        },
+                        success: function (e) {
+                            if (e.error == 0) {
+                                alert(e.msg);
+                            } else {
+                                alert('操作失败了');
+                            }
+                        }
+                    });
+                }
+            });
+        });
     </script>
-
-        <script>
-
-            var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-                lineNumbers: true,
-                styleActiveLine: true,
-                matchBrackets: true,
-                lineWrapping: true,
-                theme: "mdn-like"
-            });
-
-            var documentHeight = $(document).height();
-            editor.setSize( 'auto', documentHeight-100 );
-            //
-            // $('#menus').css({height : documentHeight-100});
-
-            $(function(){
-                let templateName = "{{ $templateName }}";
-                let validNavId = "{{$validNavId}}";
-                let token = "{{csrf_token()}}";
-                var hasRequest=null;
-                //应用本次更改
-                $('#btn-apply-change').click(function(){
-
-                    if( confirm('您即将应用本次更改，将会替换现有模板文件，是否继续?') && confirm("确定继续操作?")){
-                        if( hasRequest !=null ){
-                            hasRequest.abort();
-                        }
-
-                        hasRequest= $.ajax({
-                            url : "{{ route('rotate.template.apply') }}",
-                            type:"post" ,
-                            data : { templateName : templateName , validNavId:validNavId , templateContent : editor.getValue() , _token : token },
-                            dataType : "json" ,
-                            error: function(){
-                                alert("应用时发生错误，您可以稍后尝试重试，如果错误继续存在，请联系站点管理员~");
-                            },
-                            success : function( e ){
-                                if( e.error == 0){
-                                    alert( e.msg );
-                                    window.location.reload();
-                                }else{
-                                    alert( '操作失败了' );
-                                }
-                            }
-                        });
-                    }
-                });
-
-                $('#btn-back-last-version').click(function(){
-                    if( confirm('确定要回滚到上次版本吗? 是否继续?') && confirm("确定继续操作?")){
-                        let backVersion = "";
-                        while( !backVersion ){
-                            backVersion =  window.prompt( "请输入要回归的版本，__LAST__ 将回滚到上次版本" , "__LAST__"   );
-                            if( backVersion == null ){
-                                break;
-                            }
-                        }
-
-                        if( !backVersion){
-                            return false;
-                        }
-
-                        if( hasRequest !=null ){
-                            hasRequest.abort();
-                        }
-
-                        hasRequest= $.ajax({
-                            url : "{{ route('rotate.template.rollBack') }}",
-                            type:"post" ,
-                            data : { backVersion: backVersion,templateName : templateName , validNavId:validNavId , templateContent : editor.getValue() , _token : token },
-                            dataType : "json" ,
-                            error: function(){
-                                alert("应用时发生错误，您可以稍后尝试重试，如果错误继续存在，请联系站点管理员~");
-                            },
-                            success : function( e ){
-                                if( e.error == 0){
-                                    alert( e.msg );
-                                }else{
-                                    alert( '操作失败了' );
-                                }
-                            }
-                        });
-                    }
-                });
-            });
-        </script>
 @endsection
