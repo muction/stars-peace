c9f0358e63758c3fe0c3b7570e28372bd7351aa0
muction <muction@yeah.net>
2020-12-25 15:54:42 +0800

    增加API机制

--- Src/Controller/ApiContentController.php
+++ Src/Controller/ApiContentController.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * ------------------------------------------------------
- *
+ *  APP
  * ------------------------------------------------------
  */
 
 namespace Stars\Peace\Controller;
 use App\Http\Controllers\Controller;
-
 abstract class ApiContentController extends Controller
 {
     use ResponseContent;
+
 }
