4e9ad2c18f683f931972f6c0babff0ed2c006bc0
Haha <muction@yeah.net>
2022-10-23 13:41:59 +0800

    支持表格数据时，筛选

--- Src/Views/Bamboo/plugs/list/select.blade.php
+++ Src/Views/Bamboo/plugs/list/select.blade.php
@@ -4,10 +4,14 @@
 @if ( isset( $sheetColumns[$_listColumnName]['options'][\Stars\Peace\Foundation\SheetSheet::OPTION_KEY_VALUE_TABLE] ) )
     @php( $__sheetValueTableValue= $sheetColumns[$_listColumnName]['options'][\Stars\Peace\Foundation\SheetSheet::OPTION_KEY_VALUE_TABLE]['data'])
     @php( $__sheetValueTableField= $sheetColumns[$_listColumnName]['options'][\Stars\Peace\Foundation\SheetSheet::OPTION_KEY_VALUE_TABLE]['column'])
+    @php( $__selectValueTableDbName = $sheetColumns[$_listColumnName]['db_name'] ?? '')
 
     @foreach($__sheetValueTableValue as $_index=>$_item)
         @if(  $_item[$__sheetValueTableField['value']]  == $__selectActiveValue )
-            {{ $_item[$__sheetValueTableField['title'] ]}}
+            <a
+                href="{{ route(  'rotate.article.articles' , ['navId'=>$navId ,'menuId'=>$menuId , 'bindId'=>$bindId ,'infoId'=>$valueItem['id'] ,'_select_table_column'=>$__selectValueTableDbName,'_select_table_value_'=> $_item[$__sheetValueTableField['value']]  ] ) }}"
+            >
+                {{ $_item[$__sheetValueTableField['title'] ]}}</a>
             @break
         @endif
     @endforeach
